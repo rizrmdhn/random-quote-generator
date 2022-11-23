88ccfe28f7b6ee0654fc94dc005d6374b720433c
rizrmdhn <rizrmdhn.work@gmail.com>
2022-11-23 12:06:17 +0800

    test

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Apps from './components/Apps';
-
-import './styles/styles.css';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<Apps />)
